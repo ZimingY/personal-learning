e20c5cbd43eff4b758eeaa87acf5a5547c9ab75e
ZimingY <zimingyin@outlook.com>
2018-09-20 14:28:36 -0400

    aa

--- leetcode/problem/26 Remove Duplicates from Sorted Array/solution.py
+++ leetcode/problem/26 Remove Duplicates from Sorted Array/solution.py
@@ -1,1 +1,14 @@
+class Solution:
+    def removeDuplicates(self, A):
+        if not A:
+            return 0
 
+        N= 0
+
+        for i in range(1,len(A)):
+            if A[N] != A[i]:
+                N=N+1
+                A[N] = A[i]
+
+        return N+1
+                
