8cafd4a103556b07b4238ba75a50ba490e6d4a6f
Zimmath <30856088+ZimingY@users.noreply.github.com>
2018-09-25 23:41:58 -0400

    Update solution.py

--- leetcode/problem/27 Remove Element/solution.py
+++ leetcode/problem/27 Remove Element/solution.py
@@ -1,1 +1,19 @@
 
+class Solution:
+    def removeElement(self, nums, val):
+        start=0 
+        end = len(nums)-1
+        while start<=end:
+            if nums[start]==val:
+                nums[start]=nums[end]
+                end=end-1
+            else:
+                start+=1
+        return start
+            
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
