dea490780f19b480719f1e6a1f71b293d86abff1
Zimmath <30856088+ZimingY@users.noreply.github.com>
2018-09-25 23:51:56 -0400

    Update solution.py

--- leetcode/problem/35 Search Insert Position/solution.py
+++ leetcode/problem/35 Search Insert Position/solution.py
@@ -1,1 +1,28 @@
-
+## my solution
+class Solution:
+    def searchInsert(self, nums, target):
+        point = 0
+        for i in range(len(nums)):
+            if nums[i]==target:
+                point = i
+                break
+            elif nums[i]>target:
+                    point = i-1
+                    break
+        return point
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        
+## top solution
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """       
+        return len([x for x in nums if x<target])
