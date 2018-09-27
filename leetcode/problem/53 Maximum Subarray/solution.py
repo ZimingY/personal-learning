b2b2d784614f10af1dccf05822f28ee522adcb75
Zimmath <30856088+ZimingY@users.noreply.github.com>
2018-09-27 00:18:28 -0400

    Update solution.py

--- leetcode/problem/53 Maximum Subarray/solution.py
+++ leetcode/problem/53 Maximum Subarray/solution.py
@@ -1,1 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        maxsum =cursum= nums[0]
+        for item in nums[1:]:
+            cursum = max(item,cursum + item)
+            maxsum = max(maxsum,cursum)
+        return maxsum
+ 
 
