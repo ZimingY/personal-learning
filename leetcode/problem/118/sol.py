e1c9cd053cccc906441f85da61511251a2215db7
Zimmath <30856088+ZimingY@users.noreply.github.com>
2019-01-07 15:50:28 -0500

    Update sol.py

--- leetcode/problem/118/sol.py
+++ leetcode/problem/118/sol.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        tri = list()
+        for i in range(numRows):
+                tri.append([1]*(i+1))
+                if i>1:
+                    
+                    for j in range(1,i):
+                        tri[i][j]  = tri[i-1][j-1] + tri[i-1][j] 
+        return tri
