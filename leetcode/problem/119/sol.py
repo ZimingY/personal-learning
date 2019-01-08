2835e826eb9771660b8242dab6b611fe3c2416d6
Zimmath <30856088+ZimingY@users.noreply.github.com>
2019-01-08 14:47:08 -0500

    Update sol.py

--- leetcode/problem/119/sol.py
+++ leetcode/problem/119/sol.py
@@ -8,3 +8,9 @@ class Solution:
         for i in range(1,rowIndex):
             tri[i] = int(tri[i-1]*(rowIndex - i + 1)/i)
         return tri
+
+    
+    
+# in nth row, we have a list: [1, a(1), a(2),..., a(n)]
+
+# a(k+1)  = a(k)*(n-k)/(k+1)
